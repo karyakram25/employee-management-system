13c9863372352a5d577c582d0c24971036940b4a
Sudesh <sudeshsrajput21@gmail.com>
2026-03-28 17:45:28 +0530

    Initial commit - EMS CRUD Spring Boot project

---
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,34 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+//		Employee employee = new Employee();
+//		employee.setFirstName("Bernardo");
+//		employee.setLastName("Silva");
+//		employee.setEmailId("silva@gmail.com");
+//		employeeRepository.save(employee);
+//
+//		Employee employee1 = new Employee();
+//		employee1.setFirstName("Ryan");
+//		employee1.setLastName("Cherki");
+//		employee1.setEmailId("cherki@gmail.com");
+//		employeeRepository.save(employee1);
+	}
+}
