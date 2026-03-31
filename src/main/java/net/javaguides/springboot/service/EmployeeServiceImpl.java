30f4824465ee31f13d9991b9914a244b05f7f32d
Sudesh <sudeshsrajput21@gmail.com>
2026-03-31 11:31:54 +0530

    Added Logger in Service Layer

--- src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -5,6 +5,8 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.mapper.EmployeeMapper;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,10 +15,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     private final EmployeeRepository employeeRepository;
+    //Adding logger
+    private static final Logger log = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     //constructor injection
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
@@ -45,12 +52,21 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        //Adding Log Info
+        log.info("Creating employee with email:{} ",employeeDto.getEmailId());
+
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
+
+        log.info("Employee created with ID :{}", savedEmployee.getId());
+
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
     @Override
     public EmployeeDto getEmployeeById(Long id) {
+
+        log.info("Yo I am testing log :Fetching employee with ID: {}", id);
+
          Employee emp=employeeRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Employee does not exist with id: "+id));
         return EmployeeMapper.mapToEmployeeDto(emp);
@@ -58,8 +74,14 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public EmployeeDto updateEmployee(Long id, EmployeeDto dto) {
+
+        log.info("Updating employee with ID:{}",id);
+
         Employee emp = employeeRepository.findById(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Employee does not exist with id : "+id));
+                .orElseThrow(()-> {
+                    log.error("Employee not found with ID: {}", id);    //logging for error
+                    return new ResourceNotFoundException("Employee does not exist with id : "+id);
+                });
 
         emp.setFirstName(dto.getFirstName());
         emp.setLastName(dto.getLastName());
@@ -67,13 +89,19 @@ public class EmployeeServiceImpl implements EmployeeService{
 
         Employee updated = employeeRepository.save(emp);
 
+        log.info("Employee not found with ID: {}", id);
+
         return EmployeeMapper.mapToEmployeeDto(updated);
     }
 
     @Override
     public void deleteEmployee(Long id) {
+        log.info("Deleting employee with ID: {}", id);
+
         Employee employee= employeeRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Employee does not exist with id: "+id));
         employeeRepository.delete(employee);
+
+        log.info("Employee deleted with ID: {}",id);
     }
 }
